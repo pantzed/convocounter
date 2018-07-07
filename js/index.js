8835da2f9f0d71600c8bec8f7b9b40cbc454549b
Marc Majcher <majcher@gmail.com>
2018-07-07 17:28:03 -0500

    name form

--- js/index.js
+++ js/index.js
@@ -4,15 +4,22 @@
 /* eslint-disable no-use-before-define */
 
 (() => {
+    document.ConvoCounter = {};
+
     document.addEventListener('DOMContentLoaded', initApp);
+    document.getElementById('startButton').addEventListener('click', startTimer);
 
     function initApp() {
+        document.ConvoCounter.participantCount = 1;
         const formElement = document.getElementById('participants');
+        addPersonInputs(formElement);
+        addPersonInputs(formElement);
+        addPersonInputs(formElement);
         addPersonInputs(formElement);
     }
 
     function addPersonInputs(form) {
-        const pname = 'pname';
+        const pname = `pname${document.ConvoCounter.participantCount++}`;
         const el = appendElement(form, 'label', {
             for: pname
         });
@@ -22,6 +29,13 @@
             name: pname,
             placeholder: 'Participant Name'
         });
+    }
+
+    function startTimer(event) { // eslint-disable-line no-unused-vars
+        event.preventDefault();
+        document.getElementById('participant-inputs').style.display = 'none';
+        const inputs = [...document.querySelectorAll('input')].map(e => e.value);
+        console.log(inputs);
     }
 
     /* Create a new element of type, set given attributes,
